ab2fc5e07f1caf552c7ce7040ca6c5a0317fcaed
Rahaf <r.abualhassan@gmail.com>
2021-09-06 17:11:50 +0200

    iteration 3 done

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -6,8 +6,19 @@ const Cebelrity = require('../models/Celebrity');
 router.get('/celebrities', (req, res, next) => {
     Celebrity.find()
                 .then(celebsFromDB => {
-                    console.log(celebsFromDB);
+                    //console.log(celebsFromDB);
                     res.render('celebritiesFolder', { celebList: celebsFromDB });
+                })
+                .catch(err => {
+                    next(err);
+                })
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+    Celebrity.findById(req.params.id)
+                .then(celebFromDB => {
+                    console.log(celebFromDB);
+                    res.render('celebritiesFolder/show', { celebrity: celebFromDB});
                 })
                 .catch(err => {
                     next(err);
